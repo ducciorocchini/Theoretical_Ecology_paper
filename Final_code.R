e78cf032941381b71b033bcc0ead2532f719e32d
Duccio Rocchini <ducciorocchini@gmail.com>
2024-05-08 06:47:55 +0200

    Update Final_code.R

--- Final_code.R
+++ Final_code.R
@@ -2,9 +2,31 @@
 # Data from Sass de Stria
 
 library(terra)
+library(rasterdiv)
+library(viridis)
 
-data(volcano)
-r <- terra::rast(volcano)
-# we want to compute Rao's index on this data using a 3x3 window
-res <- paRao(x = r, window = 3, alpha = 2, method = "classic")
-terra::plot(res[[1]][[1]])
+# loading data
+sass <- rast("~/Downloads/sass.jpg")
+
+#crop 
+ext <- c(50,100,100,150)
+sassc <- crop(sass, ext)
+
+# theoretical data
+# td <- matrix(runif(100), nrow = 10)
+# plot(rast(td))
+     
+# Shannon
+sha <- ShannonP(sassc, window=1, np=1)
+plot(sha)
+
+# Shannon
+shannon <- Shannon(x=sassc,window=3)
+
+# Rao
+res <- paRao(x=sassc, window=3, alpha=3, method = "classic")
+plot(res[[1]][[1]], col=viridis(100))
+
+# stack
+stack <- c(shannon[[1]][[1]],res[[1]][[1]])
+plot(stack, col=viridis(100))
