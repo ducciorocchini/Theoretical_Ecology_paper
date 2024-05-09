98e0c3c7ecc303baf70717521db71c4f7034e387
Duccio Rocchini <ducciorocchini@gmail.com>
2024-05-09 07:30:38 +0200

    Update Code.R

--- Code.R
+++ Code.R
@@ -7,7 +7,7 @@ library(viridis)
 
 # loading data
 sass <- rast("~/Documents/rao_devst/sass.jpg")
-sent60 <- rast("~/Documents/rao_devst/sentinel-2_60m.tif")
+sent <- rast("~/Documents/rao_devst/sentinel-2_20m.tif")
 
 #crop 
 ext <- c(50,250,100,300)
@@ -18,7 +18,7 @@ sassc <- crop(sass, ext)
 # plot(rast(td))
      
 # Shannon
-shannon <- Shannon(x=sassc,window=3)
+shannon <- Shannon(x=sassc, window=3)
 
 # Rao
 res <- paRao(x=sassc, window=3, alpha=3, method = "classic")
@@ -27,3 +27,16 @@ plot(res[[1]][[1]], col=viridis(100))
 # stack
 stack <- c(shannon[[1]][[1]],res[[1]][[1]])
 plot(stack, col=viridis(100))
+
+# Sentinel data
+# Shannon
+shannon <- Shannon(x=sent, window=3)
+
+# Rao
+res <- paRao(x=sent, window=3, alpha=3, method = "classic")
+plot(res[[1]][[1]], col=viridis(100))
+
+# stack
+stack <- c(shannon[[1]][[1]],res[[1]][[1]])
+plot(stack, col=viridis(100))
+
