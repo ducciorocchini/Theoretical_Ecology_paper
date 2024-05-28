a2490dd66915e3f7ee999f61111764d9797a413b
Duccio Rocchini <ducciorocchini@gmail.com>
2024-05-28 14:48:41 +0200

    Update Code.R

--- Code.R
+++ Code.R
@@ -6,22 +6,22 @@ library(rasterdiv)
 library(viridis)
 
 # loading data
-sass <- rast("~/Documents/rao_devst/sass.jpg")
+sella <- rast("~/Documents/rao_devst/sella.jpg")
 sent <- rast("~/Documents/rao_devst/sentinel-2_20m.tif")
 
 #crop 
 ext <- c(50,250,100,300)
-sassc <- crop(sass, ext)
+sellac <- crop(sella, ext)
 
 # theoretical data
 # td <- matrix(runif(100), nrow = 10)
 # plot(rast(td))
      
 # Shannon
-shannon <- Shannon(x=sassc, window=3)
+shannon <- Shannon(x=sellac, window=3)
 
 # Rao
-res <- paRao(x=sassc, window=3, alpha=3, method = "classic")
+res <- paRao(x=sellac, window=3, alpha=3, method = "classic")
 plot(res[[1]][[1]], col=viridis(100))
 
 # stack
